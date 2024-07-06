0c54da2b19c5dddba216d35bb30d8e39f496c5da
nadiannis <annisanadianeyla@gmail.com>
2024-07-06 21:24:05 +0700

    test: test all repositories in order service

--- order-service/src/test/java/com/nadiannis/order_service/repository/OrderItemRepositoryTests.java
+++ order-service/src/test/java/com/nadiannis/order_service/repository/OrderItemRepositoryTests.java
@@ -1,11 +1,103 @@
 package com.nadiannis.order_service.repository;
 
+import com.nadiannis.order_service.entity.Order;
+import com.nadiannis.order_service.entity.OrderItem;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+@DataR2dbcTest
 public class OrderItemRepositoryTests {
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    private OrderItem orderItem1;
+
+    private OrderItem orderItem2;
+
+    private Order order;
+
+    @BeforeEach
+    void setUp() {
+        orderItem1 = OrderItem.builder()
+                .orderId(1L)
+                .productId(1L)
+                .price(10000F)
+                .quantity(10)
+                .build();
+        orderItem2 = OrderItem.builder()
+                .orderId(1L)
+                .productId(2L)
+                .price(20000F)
+                .quantity(20)
+                .build();
+        order = Order.builder()
+                .paymentMethod("CASH")
+                .billingAddress("Bekasi")
+                .shippingAddress("Jakarta")
+                .orderStatus("CREATED")
+                .totalAmount(100000F)
+                .customerId(1L)
+                .build();
+    }
+
     // Flux<OrderItem> findAll()
+    @Test
+    public void OrderItemRepository_FindAll_ReturnMoreThanOneOrderItems() {
+        orderRepository.save(order).block();
+        orderItem1.setOrderId(order.getId());
+        orderItem2.setOrderId(order.getId());
+        orderItemRepository.saveAll(Flux.just(orderItem1, orderItem2)).blockLast();
+
+        Mono<List<OrderItem>> orderItemListMono = orderItemRepository.findAll().collectList();
+
+        StepVerifier.create(orderItemListMono)
+                .assertNext(orderItemList -> {
+                    Assertions.assertThat(orderItemList).isNotNull();
+                    Assertions.assertThat(orderItemList.size()).isEqualTo(2);
+                })
+                .verifyComplete();
+    }
 
     // Flux<OrderItem> saveAll(orderItems)
+    @Test
+    public void OrderItemRepository_SaveAll_ReturnSavedOrderItems() {
+        orderRepository.save(order).block();
+        orderItem1.setOrderId(order.getId());
+        orderItem2.setOrderId(order.getId());
+        Flux<OrderItem> savedOrderItemListFlux = orderItemRepository.saveAll(Flux.just(orderItem1, orderItem2));
+
+        StepVerifier.create(savedOrderItemListFlux)
+                .expectNextMatches(orderItem -> orderItem != null && orderItem.getId().equals(orderItem1.getId()))
+                .expectNextMatches(orderItem -> orderItem != null && orderItem.getId().equals(orderItem2.getId()))
+                .verifyComplete();
+    }
 
     // Flux<OrderItem> findByOrderId(orderId)
+    @Test
+    public void OrderItemRepository_FindByOrderId_ReturnOrderItems() {
+        orderRepository.save(order).block();
+        orderItem1.setOrderId(order.getId());
+        orderItem2.setOrderId(order.getId());
+        orderItemRepository.saveAll(Flux.just(orderItem1, orderItem2)).blockLast();
+
+        Flux<OrderItem> orderItemListFlux = orderItemRepository.findByOrderId(order.getId());
+
+        StepVerifier.create(orderItemListFlux)
+                .expectNextMatches(orderItem -> orderItem != null && orderItem.getOrderId().equals(order.getId()))
+                .expectNextMatches(orderItem -> orderItem != null && orderItem.getOrderId().equals(order.getId()))
+                .verifyComplete();
+    }
 
 }
--- order-service/src/test/java/com/nadiannis/order_service/repository/OrderRepositoryTests.java
+++ order-service/src/test/java/com/nadiannis/order_service/repository/OrderRepositoryTests.java
@@ -1,13 +1,94 @@
 package com.nadiannis.order_service.repository;
 
+import com.nadiannis.order_service.entity.Order;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+@DataR2dbcTest
 public class OrderRepositoryTests {
 
+    @Autowired
+    private OrderRepository repository;
+
+    private Order order1;
+
+    private Order order2;
+
+    @BeforeEach
+    void setUp() {
+        order1 = Order.builder()
+                .paymentMethod("CASH")
+                .billingAddress("Bekasi")
+                .shippingAddress("Jakarta")
+                .orderStatus("CREATED")
+                .totalAmount(100000F)
+                .customerId(1L)
+                .build();
+        order2 = Order.builder()
+                .paymentMethod("BANK_TRANSFER")
+                .billingAddress("Bandung")
+                .shippingAddress("Semarang")
+                .orderStatus("COMPLETED")
+                .totalAmount(200000F)
+                .customerId(2L)
+                .build();
+    }
+
     // Flux<Order> findAll()
+    @Test
+    public void OrderRepository_FindAll_ReturnMoreThanOneOrders() {
+        repository.saveAll(Flux.just(order1, order2)).blockLast();
+
+        Mono<List<Order>> orderListMono = repository.findAll().collectList();
+
+        StepVerifier.create(orderListMono)
+                .assertNext(orderList -> {
+                    Assertions.assertThat(orderList).isNotNull();
+                    Assertions.assertThat(orderList.size()).isEqualTo(2);
+                })
+                .verifyComplete();
+    }
 
     // Mono<Order> save(order)
+    @Test
+    public void OrderRepository_Save_ReturnSavedOrder() {
+        Mono<Order> savedOrderMono = repository.save(order1);
+
+        StepVerifier.create(savedOrderMono)
+                .expectNextMatches(order -> order != null && order.getId().equals(order1.getId()))
+                .verifyComplete();
+    }
 
     // Mono<Order> findById(id)
+    @Test
+    public void OrderRepository_FindById_ReturnOrder() {
+        repository.save(order1).block();
+
+        Mono<Order> orderMono = repository.findById(order1.getId());
+
+        StepVerifier.create(orderMono)
+                .expectNextMatches(order -> order != null && order.getId().equals(order1.getId()))
+                .verifyComplete();
+    }
 
     // Mono<Void> deleteById(id)
+    @Test
+    public void OrderRepository_DeleteById_ReturnOrderIsEmpty() {
+        repository.save(order1).block();
+
+        Mono<Void> result = repository.deleteById(order1.getId());
+
+        StepVerifier.create(result.then(repository.findById(order1.getId())))
+                .expectNextCount(0L)
+                .verifyComplete();
+    }
 
 }
