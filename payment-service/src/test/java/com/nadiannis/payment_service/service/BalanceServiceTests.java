cc9fa72430f241eefb257ad613f9a88501bc7258
nadiannis <annisanadianeyla@gmail.com>
2024-07-07 01:57:11 +0700

    test: test methods in TransactionDetailService & BalanceService

--- payment-service/src/test/java/com/nadiannis/payment_service/service/BalanceServiceTests.java
+++ payment-service/src/test/java/com/nadiannis/payment_service/service/BalanceServiceTests.java
@@ -1,15 +1,144 @@
 package com.nadiannis.payment_service.service;
 
+import com.nadiannis.common.dto.balance.AmountUpdateReqDto;
+import com.nadiannis.common.dto.balance.BalanceResDto;
+import com.nadiannis.common.utils.AmountUpdateAction;
+import com.nadiannis.payment_service.dto.BalanceReqDto;
+import com.nadiannis.payment_service.entity.Balance;
+import com.nadiannis.payment_service.repository.BalanceRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class BalanceServiceTests {
 
+    @Mock
+    private BalanceRepository balanceRepository;
+
+    @InjectMocks
+    private BalanceService balanceService;
+
+    private Balance balance1;
+    private Balance balance2;
+    private BalanceReqDto balanceReqDto;
+    private AmountUpdateReqDto amountUpdateReqDto1;
+    private AmountUpdateReqDto amountUpdateReqDto2;
+
+    @BeforeEach
+    void setUp() {
+        balance1 = Balance.builder().id(1L).amount(1000000F).customerId(1L).build();
+        balance2 = Balance.builder().id(2L).amount(2000000F).customerId(2L).build();
+        balanceReqDto = BalanceReqDto.builder().amount(1000000F).customerId(1L).build();
+        amountUpdateReqDto1 = AmountUpdateReqDto.builder().action(AmountUpdateAction.DEBIT.toString()).amount(100000F).build();
+        amountUpdateReqDto2 = AmountUpdateReqDto.builder().action(AmountUpdateAction.CREDIT.toString()).amount(100000F).build();
+    }
+
     // Flux<BalanceResDto> getAll()
+    @Test
+    public void BalanceService_GetAll_ReturnBalanceResDtos() {
+        when(balanceRepository.findAll()).thenReturn(Flux.just(balance1, balance2));
+
+        Mono<List<BalanceResDto>> balanceListMono = balanceService.getAll().collectList();
+
+        StepVerifier.create(balanceListMono)
+                .assertNext(balanceList -> {
+                    Assertions.assertThat(balanceList).isNotNull();
+                    Assertions.assertThat(balanceList.size()).isEqualTo(2);
+                })
+                .verifyComplete();
+
+        verify(balanceRepository, times(1)).findAll();
+    }
 
     // Mono<BalanceResDto> add(BalanceReqDto balanceReqDto)
+    @Test
+    public void BalanceService_Add_SaveAndReturnBalanceResDto() {
+        when(balanceRepository.save(any(Balance.class))).thenReturn(Mono.just(balance1));
+
+        Mono<BalanceResDto> savedBalanceMono = balanceService.add(balanceReqDto);
+
+        StepVerifier.create(savedBalanceMono)
+                .expectNextMatches(savedBalance -> savedBalance != null && savedBalance.getId().equals(balance1.getId()))
+                .verifyComplete();
+
+        verify(balanceRepository, times(1)).save(any(Balance.class));
+    }
 
     // Mono<BalanceResDto> getByCustomerId(Long customerId)
+    @Test
+    public void BalanceService_GetByCustomerId_ReturnBalanceResDto() {
+        Long customerId = balance1.getCustomerId();
+        when(balanceRepository.findByCustomerId(anyLong())).thenReturn(Mono.just(balance1));
+
+        Mono<BalanceResDto> balanceMono = balanceService.getByCustomerId(customerId);
+
+        StepVerifier.create(balanceMono)
+                .expectNextMatches(balance -> balance != null && balance.getCustomerId().equals(customerId))
+                .verifyComplete();
+
+        verify(balanceRepository, times(1)).findByCustomerId(anyLong());
+    }
 
     // Mono<BalanceResDto> updateAmount(Long customerId, AmountUpdateReqDto amountUpdateReqDto)
+    @Test
+    public void BalanceService_DebitAmount_DebitAmountAndReturnBalanceResDto() {
+        Long customerId = balance1.getCustomerId();
+        Float updatedAmount = balance1.getAmount() - amountUpdateReqDto1.getAmount();
+        when(balanceRepository.findByCustomerId(anyLong())).thenReturn(Mono.just(balance1));
+        when(balanceRepository.save(any(Balance.class))).thenReturn(Mono.just(balance1));
+
+        Mono<BalanceResDto> updatedBalanceMono = balanceService.updateAmount(customerId, amountUpdateReqDto1);
+
+        StepVerifier.create(updatedBalanceMono)
+                .expectNextMatches(updatedBalance -> updatedBalance != null && updatedBalance.getAmount().equals(updatedAmount))
+                .verifyComplete();
+
+        verify(balanceRepository, times(1)).findByCustomerId(anyLong());
+        verify(balanceRepository, times(1)).save(any(Balance.class));
+    }
+
+    @Test
+    public void BalanceService_CreditAmount_CreditAmountAndReturnBalanceResDto() {
+        Long customerId = balance1.getCustomerId();
+        Float updatedAmount = balance1.getAmount() + amountUpdateReqDto2.getAmount();
+        when(balanceRepository.findByCustomerId(anyLong())).thenReturn(Mono.just(balance1));
+        when(balanceRepository.save(any(Balance.class))).thenReturn(Mono.just(balance1));
+
+        Mono<BalanceResDto> updatedBalanceMono = balanceService.updateAmount(customerId, amountUpdateReqDto2);
+
+        StepVerifier.create(updatedBalanceMono)
+                .expectNextMatches(updatedBalance -> updatedBalance != null && updatedBalance.getAmount().equals(updatedAmount))
+                .verifyComplete();
+
+        verify(balanceRepository, times(1)).findByCustomerId(anyLong());
+        verify(balanceRepository, times(1)).save(any(Balance.class));
+    }
 
     // Mono<Void> deleteByCustomerId(Long customerId)
+    @Test
+    public void BalanceService_DeleteByCustomerId_ReturnBalanceIsEmpty() {
+        Long customerId = balance1.getCustomerId();
+        when(balanceRepository.findByCustomerId(anyLong())).thenReturn(Mono.just(balance1));
+        when(balanceRepository.deleteByCustomerId(anyLong())).thenReturn(Mono.empty());
+
+        Mono<Void> result = balanceService.deleteByCustomerId(customerId);
+
+        StepVerifier.create(result).verifyComplete();
+
+        verify(balanceRepository, times(1)).findByCustomerId(anyLong());
+        verify(balanceRepository, times(1)).deleteByCustomerId(anyLong());
+    }
 
 }
--- payment-service/src/test/java/com/nadiannis/payment_service/service/TransactionDetailServiceTests.java
+++ payment-service/src/test/java/com/nadiannis/payment_service/service/TransactionDetailServiceTests.java
@@ -1,15 +1,176 @@
 package com.nadiannis.payment_service.service;
 
+import com.nadiannis.common.dto.balance.AmountUpdateReqDto;
+import com.nadiannis.common.dto.balance.BalanceResDto;
+import com.nadiannis.common.dto.transactiondetail.TransactionDetailAddReqDto;
+import com.nadiannis.common.dto.transactiondetail.TransactionDetailResDto;
+import com.nadiannis.common.utils.Mode;
+import com.nadiannis.common.utils.TransactionDetailStatus;
+import com.nadiannis.payment_service.dto.TransactionDetailUpdateReqDto;
+import com.nadiannis.payment_service.entity.TransactionDetail;
+import com.nadiannis.payment_service.repository.TransactionDetailRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class TransactionDetailServiceTests {
 
+    @Mock
+    private TransactionDetailRepository transactionDetailRepository;
+
+    @Mock
+    private BalanceService balanceService;
+
+    @InjectMocks
+    private TransactionDetailService transactionDetailService;
+
+    private TransactionDetail transactionDetail1;
+    private TransactionDetail transactionDetail2;
+    private TransactionDetailAddReqDto transactionDetailAddReqDto;
+    private TransactionDetailUpdateReqDto transactionDetailUpdateReqDto;
+    private BalanceResDto balanceResDto;
+
+    @BeforeEach
+    void setUp() {
+        transactionDetail1 = TransactionDetail.builder()
+                .id(1L)
+                .orderId(1L)
+                .amount(100000F)
+                .mode(Mode.CASH.toString())
+                .status(TransactionDetailStatus.APPROVED.toString())
+                .referenceNumber(UUID.randomUUID().toString())
+                .build();
+        transactionDetail2 = TransactionDetail.builder()
+                .id(2L)
+                .orderId(2L)
+                .amount(200000F)
+                .mode(Mode.BANK_TRANSFER.toString())
+                .status(TransactionDetailStatus.REJECTED.toString())
+                .referenceNumber(UUID.randomUUID().toString())
+                .build();
+        transactionDetailAddReqDto = TransactionDetailAddReqDto.builder()
+                .orderId(1L)
+                .customerId(1L)
+                .amount(100000F)
+                .mode(Mode.CASH.toString())
+                .build();
+        transactionDetailUpdateReqDto = TransactionDetailUpdateReqDto.builder()
+                .orderId(1L)
+                .amount(100000F)
+                .mode(Mode.CASH.toString())
+                .status(TransactionDetailStatus.APPROVED.toString())
+                .referenceNumber(UUID.randomUUID().toString())
+                .build();
+        balanceResDto = BalanceResDto.builder().id(1L).amount(1000000F).customerId(1L).build();
+    }
+
     // Flux<TransactionDetailResDto> getAll()
+    @Test
+    public void TransactionDetailService_GetAll_ReturnTransactionDetailResDtos() {
+        when(transactionDetailRepository.findAll()).thenReturn(Flux.just(transactionDetail1, transactionDetail2));
+
+        Mono<List<TransactionDetailResDto>> transactionDetailListMono = transactionDetailService.getAll().collectList();
+
+        StepVerifier.create(transactionDetailListMono)
+                .assertNext(transactionDetailList -> {
+                    Assertions.assertThat(transactionDetailList).isNotNull();
+                    Assertions.assertThat(transactionDetailList.size()).isEqualTo(2);
+                })
+                .verifyComplete();
+
+        verify(transactionDetailRepository, times(1)).findAll();
+    }
 
     // Mono<TransactionDetailResDto> add(TransactionDetailAddReqDto transactionDetailAddReqDto)
+    @Test
+    public void TransactionDetailService_Add_SaveAndReturnTransactionDetailResDto() {
+        when(balanceService.updateAmount(anyLong(), any(AmountUpdateReqDto.class))).thenReturn(Mono.just(balanceResDto));
+        when(transactionDetailRepository.save(any(TransactionDetail.class))).thenReturn(Mono.just(transactionDetail1));
+
+        Mono<TransactionDetailResDto> savedTransactionDetailMono = transactionDetailService.add(transactionDetailAddReqDto);
+
+        StepVerifier.create(savedTransactionDetailMono)
+                .assertNext(savedTransactionDetail -> {
+                    Assertions.assertThat(savedTransactionDetail).isNotNull();
+                    Assertions.assertThat(savedTransactionDetail.getId()).isEqualTo(transactionDetail1.getId());
+                    Assertions.assertThat(savedTransactionDetail.getOrderId()).isEqualTo(transactionDetailAddReqDto.getOrderId());
+                    Assertions.assertThat(savedTransactionDetail.getAmount()).isEqualTo(transactionDetailAddReqDto.getAmount());
+                    Assertions.assertThat(savedTransactionDetail.getMode()).isEqualTo(transactionDetailAddReqDto.getMode());
+                    Assertions.assertThat(savedTransactionDetail.getStatus()).isEqualTo(TransactionDetailStatus.APPROVED.toString());
+                })
+                .verifyComplete();
+
+        verify(balanceService, times(1)).updateAmount(anyLong(), any(AmountUpdateReqDto.class));
+        verify(transactionDetailRepository, times(1)).save(any(TransactionDetail.class));
+    }
 
     // Mono<TransactionDetailResDto> getById(Long id)
+    @Test
+    public void TransactionDetailService_GetById_ReturnTransactionDetailResDto() {
+        Long transactionDetail1Id = transactionDetail1.getId();
+        when(transactionDetailRepository.findById(anyLong())).thenReturn(Mono.just(transactionDetail1));
+
+        Mono<TransactionDetailResDto> transactionDetailMono = transactionDetailService.getById(transactionDetail1Id);
+
+        StepVerifier.create(transactionDetailMono)
+                .expectNextMatches(transactionDetail -> transactionDetail != null && transactionDetail.getId().equals(transactionDetail1Id))
+                .verifyComplete();
+
+        verify(transactionDetailRepository, times(1)).findById(anyLong());
+    }
 
     // Mono<TransactionDetailResDto> update(Long id, TransactionDetailUpdateReqDto transactionDetailUpdateReqDto)
+    @Test
+    public void TransactionDetailService_Update_UpdateAndReturnTransactionDetailResDto() {
+        Long transactionDetailId = transactionDetail2.getId();
+        transactionDetail1.setId(transactionDetailId);
+        transactionDetail1.setReferenceNumber(transactionDetailUpdateReqDto.getReferenceNumber());
+        when(transactionDetailRepository.findById(anyLong())).thenReturn(Mono.just(transactionDetail2));
+        when(transactionDetailRepository.save(any(TransactionDetail.class))).thenReturn(Mono.just(transactionDetail1));
+
+        Mono<TransactionDetailResDto> updatedTransactionDetailMono = transactionDetailService.update(transactionDetailId, transactionDetailUpdateReqDto);
+
+        StepVerifier.create(updatedTransactionDetailMono)
+                .assertNext(updatedTransactionDetail -> {
+                    Assertions.assertThat(updatedTransactionDetail).isNotNull();
+                    Assertions.assertThat(updatedTransactionDetail.getOrderId()).isEqualTo(transactionDetailUpdateReqDto.getOrderId());
+                    Assertions.assertThat(updatedTransactionDetail.getAmount()).isEqualTo(transactionDetailUpdateReqDto.getAmount());
+                    Assertions.assertThat(updatedTransactionDetail.getMode()).isEqualTo(transactionDetailUpdateReqDto.getMode());
+                    Assertions.assertThat(updatedTransactionDetail.getStatus()).isEqualTo(transactionDetailUpdateReqDto.getStatus());
+                    Assertions.assertThat(updatedTransactionDetail.getReferenceNumber()).isEqualTo(transactionDetailUpdateReqDto.getReferenceNumber());
+                })
+                .verifyComplete();
+
+        verify(transactionDetailRepository, times(1)).findById(anyLong());
+        verify(transactionDetailRepository, times(1)).save(any(TransactionDetail.class));
+    }
 
     // Mono<Void> delete(Long id)
+    @Test
+    public void TransactionDetailService_Delete_ReturnTransactionDetailIsEmpty() {
+        Long transactionDetailId = transactionDetail1.getId();
+        when(transactionDetailRepository.findById(anyLong())).thenReturn(Mono.just(transactionDetail1));
+        when(transactionDetailRepository.deleteById(anyLong())).thenReturn(Mono.empty());
+
+        Mono<Void> result = transactionDetailService.delete(transactionDetailId);
+
+        StepVerifier.create(result).verifyComplete();
+
+        verify(transactionDetailRepository, times(1)).findById(anyLong());
+        verify(transactionDetailRepository, times(1)).deleteById(anyLong());
+    }
 
 }
